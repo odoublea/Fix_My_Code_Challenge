ad4db5b80391e3e43fe40f99d2e47e6cf44b0d92
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-05-14 17:25:49 +0000

    Validate square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,13 +2,15 @@
 
 class square():
     
-    width = 0
-    height = 0
-
     
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.width = kwargs.get('width', 0)
+        self.height = kwargs.get('height', 0)
+
+        if self.width != self.height:
+            raise ValueError("Width and height must be the same")
+        #for key, value in kwargs.items():
+            #setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -21,8 +23,7 @@ class square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
