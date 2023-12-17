7e2d7fd1892991f2c23ab07e2bac79f7ab0e9b68
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-17 21:31:47 +0100

    my-square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -38,7 +38,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_Square())
     print(s.PermiterOfMySquare())
