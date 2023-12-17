612769f6473066f14cf0d90d4a026982063be114
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-17 09:10:22 +0100

    email setter finetune

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -6,25 +6,22 @@ User class
 class User():
     """ Documentation """
 
-    def __init__(self, email=None):
+    def __init__(self):
         """ Documentation """
         self.__email = None
-        if email:
-            self.email = email
-    
+
     @property
     def email(self):
         """ Documentation """
         return self.__email
-
-
+   
     @email.setter
     def email(self, value):
         """ Documentation """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-   
+
     
 if __name__ == "__main__":
 
