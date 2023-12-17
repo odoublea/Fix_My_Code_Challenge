25b23216f2f0da70b58afde770daf18b917f0fcd
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-17 09:02:05 +0100

    reverse redefine constructor

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,25 +4,26 @@
 class Square():
     """ Square class """
     
-    def __init__(self, width):
+    def __init__(self, width, height):
         """ Initialize the square class """
         self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """ Perimeter of the square"""
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String representation of the square"""
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
