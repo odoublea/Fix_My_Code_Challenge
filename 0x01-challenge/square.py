96f19a70306c3aa9c0551711b60f245839994c6c
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-17 08:42:17 +0100

    Declare perfect square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""The Square Module"""
 
-class square():
-    """A square class"""
+class Square():
     
-    def __init__(self, width):
-        """Initialize class constructor"""
-        try:
-            self.width = width
-        except Exception as e:
-            raise TypeError("width must be an integer")
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
